8507b06cba58b662eb7fc9d00e1ba2f002ea45b7
Calebe Tadeu Matos da Silva <calebe.silva@efrete.com.br>
2023-02-20 13:36:41 -0300

    Functions in dart code

--- bin/learning_dart_oo.dart
+++ bin/learning_dart_oo.dart
@@ -1,5 +1,29 @@
 import 'package:learning_dart_oo/learning_dart_oo.dart' as learning_dart_oo;
 
-void main(List<String> arguments) {
-  print('Hello world: ${learning_dart_oo.calculate()}!');
+void main() {
+  String nome = "Laranja";
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e crítica";
+  int diasDesdeColheita = 40;
+//  bool isMadura=funcIsMadura(diasDesdeColheita);
+  //print(isMadura);:
+  print(showsIsMadura(nome,diasDesdeColheita,cor:cor));
 }
+
+showsIsMadura(String name,int dias, {required String cor}){
+  if(dias>=30){
+    print("$name está madura");
+  }else{
+    print("$name não está madura");
+  }
+  if(cor != null){
+    print("Sua cor é $cor");
+  }
+}
+
+bool funcIsMadura(int dias){
+  return dias>=30;
+}
+
+
