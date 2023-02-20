0db183605693cad8077d4c7f840d2695dff579c1
Calebe Tadeu Matos da Silva <calebe.silva@efrete.com.br>
2023-02-20 09:13:01 -0300

    first commit

---
+++ bin/learning_dart_oo.dart
@@ -0,0 +1,5 @@
+import 'package:learning_dart_oo/learning_dart_oo.dart' as learning_dart_oo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learning_dart_oo.calculate()}!');
+}
---
+++ test/learning_dart_oo_test.dart
@@ -0,0 +1,8 @@
+import 'package:learning_dart_oo/learning_dart_oo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
